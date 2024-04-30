7931447e490aeb54bfc8f179b1237015b664f511
Amadeo Cabanela <acabanela@dons.usfca.edu>
2024-04-30 01:54:00 -0700

    Version 1: client-side webcam webapp

--- flask_deepface_emotion_detection/app.py
+++ flask_deepface_emotion_detection/app.py
@@ -13,6 +13,27 @@ CORS(app)  # Enable CORS for all routes
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    """
+    About Section.
+    """
+    return render_template('about.html')
+
+@app.route('/learning_center')
+def learning_center():
+    """
+    Learning Center Page
+    """
+    return render_template('learning_center.html')
+
+@app.route('/webcam')
+def webcam():
+    """
+    Webcam page for real-time emotion detection from facial epxressions.
+    """
+    return render_template('webcam.html')
+
 @app.route('/analyze', methods=['POST'])
 def analyze():
     data = request.get_json()
@@ -23,60 +44,9 @@ def analyze():
     img_np = np.array(img)  # Convert PIL Image to numpy array
 
     result = DeepFace.analyze(img_np, actions=['emotion'])
-    # Check if result is a list
-    # if isinstance(result, list):
-    #     for r in result:
-    #         dominant_emotion = r['dominant_emotion'][:]
-    #         print('Dominant Emotion:', dominant_emotion)
-    # else:
-    #     dominant_emotion = result['dominant_emotion'][:]
-    #     print('Dominant Emotion:', dominant_emotion)
 
     return jsonify(result)
 
-# @app.route('/analyze', methods=['POST'])
-# def analyze():
-#     imgFile = request.files['image']  # Retrieve the uploaded file
-#     img = Image.open(imgFile)  # Open the image file using PIL
-#     img = np.array(img)  # Convert the image to a numpy array
-
-#     # Convert numpy array back to PIL Image object
-#     img_pil = Image.fromarray(img)
-
-#     # Convert PIL Image object to base64 string
-#     img_base64 = image_to_base64(img_pil)
-
-#     # Analyze the base64-encoded image data
-#     result = DeepFace.analyze(img_path=img_base64, actions=['emotion'])
-
-#     return jsonify(result)
-
-
-# @app.route('/analyze', methods=['POST'])
-# def analyze():
-#     imgFile = request.files['image']  # Retrieve the uploaded file
-#     img = Image.open(imgFile)  # Open the image file using PIL
-#     img = np.array(img)  # Convert the image to a numpy array
-
-#     # Convert numpy array back to PIL Image object
-#     img_pil = Image.fromarray(img)
-
-#     # Convert PIL Image object to base64 string
-#     img_base64 = image_to_base64(img_pil)
-
-#     # print("Base64 Image:", img_base64)  # Print the base64 image data
-
-#     # Decode the base64 string
-#     img_data = base64.b64decode(img_base64.split(",")[1])
-
-#     # Save the decoded data as an image file
-#     with open('decoded_image.png', 'wb') as f:
-#         f.write(img_data)
-
-#     # Analyze the image data
-#     result = DeepFace.analyze(img_path=img_base64, actions=['emotion'])
-
-#     return jsonify(result)
 
 def image_to_base64(pil_image):
     # Convert PIL Image to base64 string (JPEG format)
@@ -86,25 +56,5 @@ def image_to_base64(pil_image):
     return f"data:image/jpeg;base64,{img_str}"
 
 
-# def image_to_base64(pil_image):
-#     # Convert PIL Image to base64 string
-#     buffered = BytesIO()
-#     pil_image.save(buffered, format="PNG")
-#     img_str = base64.b64encode(buffered.getvalue()).decode()
-#     return f"data:image/png;base64,{img_str}"
-
-# @app.route('/analyze', methods=['POST'])
-# def analyze():
-#     imgData = request.json['imgData']
-#     # result = DeepFace.analyze(img_path=imgData, actions=['emotion'])
-#     # result = DeepFace.analyze(img_path=imgData, actions=['emotion'], enforce_detection=False)
-#     result = DeepFace.analyze(img_path=imgData, actions=['emotion'],
-#                               enforce_detection=False,
-#                               detector_backend="opencv",
-#                               align=True,
-#                               silent=False)
-#     return jsonify(result)
-
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=4999, debug=True)
     app.run(debug=True)
--- flask_deepface_emotion_detection/static/script.js
+++ flask_deepface_emotion_detection/static/script.js
@@ -3,6 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
 
+    // Define the dictionary mapping emotions to emoji image paths
+    const emojiDict = {
+        "angry": "static/emojis/angry.png",
+        "fear": "static/emojis/fear.png",
+        "neutral": "static/emojis/neutral.png",
+        "sad": "static/emojis/sad.png",
+        "disgust": "static/emojis/disgust.png",
+        "happy": "static/emojis/happy.png",
+        "surprise": "static/emojis/surprised.png"
+    };
+
     async function setupCamera() {
         const stream = await navigator.mediaDevices.getUserMedia({ video: true });
         video.srcObject = stream;
@@ -24,10 +35,20 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(data => {
                 const dominant_emotion = data[0]['dominant_emotion']; // Assuming result is a list
                 const emotionLabel = document.getElementById('emotionLabel');
+                const emojiImg = document.getElementById('emojiImg');
+
+                // Set the text content of the emotion label
                 emotionLabel.textContent = 'Emotion: ' + dominant_emotion;
+
+                // Set the source of the emoji image based on the dominant emotion
+                if (emojiDict.hasOwnProperty(dominant_emotion)) {
+                    emojiImg.src = emojiDict[dominant_emotion];
+                } else {
+                    emojiImg.src = ''; // Clear the image source if emotion is not found in the dictionary
+                }
             })
             .catch(error => console.error('Error:', error));
-        }, 3000); // Adjusted interval for testing
+        }, 6000); // Adjusted interval for capturing webcam img to be analyzed - 6 seconds
     }
 
     setupCamera();
